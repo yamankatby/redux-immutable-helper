61ad9afcb6694eb7b4639315f34e379d54f300a7
Yaman KATBY <m.yaman.katby@gmail.com>
2019-05-07 15:50:44 +0300

    Remove the support of the object function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 // created with ❤ by Yaman Katby at 25 Mar 2019.
 
 import array from './array';
-import object from './object';
 
-export { array, object };
+export { array };
--- src/types/object.ts
+++
@@ -1,5 +0,0 @@
-export interface IObject<T> {
-  update: (target: ITarget<T | any>) => T;
-}
-
-export type ITarget<T> = T | ((prevSource: T) => T);
